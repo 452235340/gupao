4ec64b38492ff94fb5e60ca00917ad3adf5f3f97
452235340 <452235340@qq.com>
2019-03-10 14:04:29 +0800

    单利模式code

---
+++ designPattern/singleton/src/main/java/com/gupao/vip/pattern/singlerton/lazy/ExcutorThread.java
@@ -0,0 +1,13 @@
+package com.gupao.vip.pattern.singlerton.lazy;
+
+/**
+ * Created by qingbowu on 2019/3/10.
+ */
+public class ExcutorThread implements Runnable {
+
+    @Override
+    public void run() {
+        LazySingleton lazySingleton =LazySingleton.getInstance();
+        System.out.println(Thread.currentThread().getName()+"-->"+lazySingleton.toString());
+    }
+}
