c23dc522e90311351e73ffb5f073e36a24bd33b6
SYNACAST\v-qingbowu <v-qingbowu@pptv.com>
2019-03-12 18:23:47 +0800

    commit

--- designPattern/proxypattern/src/main/java/com/gupao/vip/dynamicproxy/jdkproxy/BuyHouseProxyTest.java
+++ designPattern/proxypattern/src/main/java/com/gupao/vip/dynamicproxy/jdkproxy/BuyHouseProxyTest.java
@@ -1,5 +1,10 @@
 package com.gupao.vip.dynamicproxy.jdkproxy;
 
+import sun.misc.ProxyGenerator;
+
+import java.io.FileOutputStream;
+import java.lang.reflect.Proxy;
+
 /**
  * Created by qingbowu on 2019/3/11.
  */
@@ -11,7 +16,15 @@ public class BuyHouseProxyTest {
             Intermediary intermediary = new Intermediary();
 
             BuyHouse customerProxy = (BuyHouse)intermediary.getInstance(customer);
+            System.out.println(customerProxy.getClass());
             customerProxy.buyHouse();
+
+
+            byte[] bytes = ProxyGenerator.generateProxyClass("$Proxy0", new Class[]{BuyHouse.class});
+            FileOutputStream fos = new FileOutputStream("$Proxy0.class");
+            fos.write(bytes);
+            fos.flush();
+            fos.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
